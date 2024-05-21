ab5e31dbf6b6ccbd0a75863f44a9b57ae34639da
Joseph K. Dugbatey <edinjoey@gmail.com>
2024-05-21 14:27:25 +0000

    Include additional general icons (batch 2)

--- lib/src/icons.dart
+++ lib/src/icons.dart
@@ -159,4 +159,152 @@ class EjaraIcons {
 
   // download-03 icon
   static const download3 = EjaraIcons._('assets/icons/download-03.svg');
+
+  // download-04 icon
+  static const download4 = EjaraIcons._('assets/icons/download-04.svg');
+
+  // download-cloud-01 icon
+  static const downloadCloud1 =
+      EjaraIcons._('assets/icons/download-cloud-01.svg');
+
+  // download-cloud-02 icon
+  static const downloadCloud2 =
+      EjaraIcons._('assets/icons/download-cloud-02.svg');
+
+  // edit-01 icon
+  static const edit1 = EjaraIcons._('assets/icons/edit-01.svg');
+
+  // edit-02 icon
+  static const edit2 = EjaraIcons._('assets/icons/edit-02.svg');
+
+  // edit-03 icon
+  static const edit3 = EjaraIcons._('assets/icons/edit-03.svg');
+
+  // edit-04 icon
+  static const edit4 = EjaraIcons._('assets/icons/edit-04.svg');
+
+  // edit-05 icon
+  static const edit5 = EjaraIcons._('assets/icons/edit-05.svg');
+
+  // equal-not icon
+  static const equalNot = EjaraIcons._('assets/icons/equal-not.svg');
+
+  // equal icon
+  static const equal = EjaraIcons._('assets/icons/equal.svg');
+
+  // eye-off icon
+  static const eyeOff = EjaraIcons._('assets/icons/eye-off.svg');
+
+  // eye icon
+  static const eye = EjaraIcons._('assets/icons/eye.svg');
+
+  // filter-funnel-01 icon
+  static const filterFunnel1 =
+      EjaraIcons._('assets/icons/filter-funnel-01.svg');
+
+  // filter-funnel-02 icon
+  static const filterFunnel2 =
+      EjaraIcons._('assets/icons/filter-funnel-02.svg');
+
+  // filter-lines icon
+  static const filterLines = EjaraIcons._('assets/icons/filter-lines.svg');
+
+  // google-chrome icon
+  static const googleChrome = EjaraIcons._('assets/icons/google-chrome.svg');
+
+  // hash-01 icon
+  static const hash1 = EjaraIcons._('assets/icons/hash-01.svg');
+
+  // hash-02 icon
+  static const hash2 = EjaraIcons._('assets/icons/hash-02.svg');
+
+  // heart-circle icon
+  static const heartCircle = EjaraIcons._('assets/icons/heart-circle.svg');
+
+  // heart-hand icon
+  static const heartHand = EjaraIcons._('assets/icons/heart-hand.svg');
+
+  // heart-hexagon icon
+  static const heartHexagon = EjaraIcons._('assets/icons/heart-hexagon.svg');
+
+  // heart-octagon icon
+  static const heartOctagon = EjaraIcons._('assets/icons/heart-octagon.svg');
+
+  // heart-rounded icon
+  static const heartRounded = EjaraIcons._('assets/icons/heart-rounded.svg');
+
+  // heart-square icon
+  static const heartSquare = EjaraIcons._('assets/icons/heart-square.svg');
+
+  // heart icon
+  static const heart = EjaraIcons._('assets/icons/heart.svg');
+
+  // hearts icon
+  static const hearts = EjaraIcons._('assets/icons/hearts.svg');
+
+  // help-circle icon
+  static const helpCircle = EjaraIcons._('assets/icons/help-circle.svg');
+
+  // help-octagon-1 icon
+  static const helpOctagon1 = EjaraIcons._('assets/icons/help-octagon-1.svg');
+
+  // help-square icon
+  static const helpSquare = EjaraIcons._('assets/icons/help-square.svg');
+
+  // home-01 icon
+  static const home1 = EjaraIcons._('assets/icons/home-01.svg');
+
+  // home-02 icon
+  static const home2 = EjaraIcons._('assets/icons/home-02.svg');
+
+  // home-03 icon
+  static const home3 = EjaraIcons._('assets/icons/home-03.svg');
+
+  // home-04 icon
+  static const home4 = EjaraIcons._('assets/icons/home-04.svg');
+
+  // home-05 icon
+  static const home5 = EjaraIcons._('assets/icons/home-05.svg');
+
+  // home-line icon
+  static const homeLine = EjaraIcons._('assets/icons/home-line.svg');
+
+  // home-smileicon
+  static const homeSmile = EjaraIcons._('assets/icons/home-smile.svg');
+
+  // info-circle icon
+  static const infoCircle = EjaraIcons._('assets/icons/info-circle.svg');
+
+  // info-hexagon icon
+  static const infoHexagon = EjaraIcons._('assets/icons/info-hexagon.svg');
+
+  // info-octagon icon
+  static const infoOctagon = EjaraIcons._('assets/icons/info-octagon.svg');
+
+  // info-square icon
+  static const infoSquare = EjaraIcons._('assets/icons/info-square.svg');
+
+  // life-buoy-01 icon
+  static const lifeBuoy1 = EjaraIcons._('assets/icons/life-buoy-01.svg');
+
+  // life-buoy-02 icon
+  static const lifeBuoy2 = EjaraIcons._('assets/icons/life-buoy-02.svg');
+
+  // link-01 icon
+  static const link1 = EjaraIcons._('assets/icons/link-01.svg');
+
+  // link-02 icon
+  static const link2 = EjaraIcons._('assets/icons/link-02.svg');
+
+  // link-03 icon
+  static const link3 = EjaraIcons._('assets/icons/link-03.svg');
+
+  // link-04 icon
+  static const link4 = EjaraIcons._('assets/icons/link-04.svg');
+
+  // link-05 icon
+  static const link5 = EjaraIcons._('assets/icons/link-05.svg');
+
+  // link-broken-01 icon
+  static const linkBroken1 = EjaraIcons._('assets/icons/link-broken-01.svg');
 }
